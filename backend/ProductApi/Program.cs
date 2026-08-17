76e1be97d6dbe2d6afe1cb0d8fe498f069a48aa4
muandiz-kaan <kaanak38@icloud.com>
2026-08-17 19:32:38 +0300

    CORS policy added for frontend integration

--- backend/ProductApi/Program.cs
+++ backend/ProductApi/Program.cs
@@ -1,11 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using ProductApi.Data;
+using ProductApi.Repositories;
+using ProductApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add DbContext 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowNextApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -19,6 +34,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseCors("AllowNextApp");
 
 app.UseAuthorization();
 
