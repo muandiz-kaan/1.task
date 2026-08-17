8722fc6bc8e888bc4fed7c44d20f60f7f1fe9e71
muandiz-kaan <kaanak38@icloud.com>
2026-08-18 01:54:45 +0300

    Swagger/OpenAPI documentation for API endpoints has integrated

--- backend/Program.cs
+++ backend/Program.cs
@@ -25,6 +25,8 @@ builder.Services.AddCors(options =>
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -33,6 +35,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseCors("AllowNextApp");
